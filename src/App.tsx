d8c1f434a43404da6e8b3e492ac050618206c9fe
NoaRiaboy <noa290303@gmail.com>
2023-01-26 16:22:05 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+import Sidebar from './components/Sidebar'
+
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Recordings from './pages/Recordings';
+import Settings from './pages/Settings';
+import Record from './pages/Record';
+
+
+
+const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+        <Router>
+          <Sidebar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/Search' element={<Search />} />
+            <Route path='/Recordings' element={<Recordings />} />
+            <Route path='/Settings' element={<Settings />} />
+              <Route path='/Recordings/:id/:filename' element={<Record />} />
+          </Routes>
+        </Router>
+      </>
+  )
 }
 
-export default App;
+export default App
