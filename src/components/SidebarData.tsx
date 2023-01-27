fe4f86f040abab1550700e54309352391e252bf8
NoaRiaboy <noa290303@gmail.com>
2023-01-27 23:32:26 +0200

    Add Sidebar

---
+++ src/components/SidebarData.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa'
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <FaIcons.FaHome />
+    },
+    {
+        title: 'Search',
+        path: '/search',
+        icon: <FaIcons.FaSearch />
+    },
+    {
+        title: 'Recordings',
+        path: '/recordings',
+        icon: <FaIcons.FaStopCircle />
+    },
+    {
+        title: 'Settings',
+        path: '/settings',
+        icon: <FaIcons.FaCog />
+    }
+]
+
